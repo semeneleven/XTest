44273e3183c75d149a7fb475cab2cd7bf33d94e3
SemenTsinsky <semen.tsinsky@gmail.com>
2018-10-03 22:49:24 +0300

    add generate_to_decode method to berger

--- codes/others/berger.py
+++ codes/others/berger.py
@@ -45,43 +45,14 @@ def generate_for_encode():
 	return data
 
 
-# def generate_for_decode():
+def generate_for_decode():
+	data = generate_for_encode()
+	r = berger(data)
+	return [data + r, len(r)]
 
-
+#test
 print(assert_code('1001', '100101'))
 print(assert_decode(['100101', 2], True))
 print(generate_for_encode())
+print(generate_for_decode())
 
-
-def ddc(N, base):
-
-	if base==[8,4,2,1]:
-
-		ddc = int()
-		for i in str(N):
-			ddc *=16
-			ddc +=int(i)
-
-		return str(bin(ddc))[2:]
-
-	else:
-
-		ddc=str()
-
-		for i in str(N):
-
-			four = [0,0,0,0]
-			current_number=int(i)
-
-			for j in range(len(base)):
-				if current_number>=base[j]:
-					current_number-=base[j]
-					four[j]=1
-
-			for j in four:
-				ddc+=str(j)
-
-		return ddc
-
-
-print(ddc(851, [7, 4, 2, 1]))
