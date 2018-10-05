11e26a31c14bd19901ff9470e2fab296ed19b2b5
semen <semen.tsinsky@gmail.com>
2018-10-05 04:08:05 +0300

    resolve #1 assert and generate mathods to shannon-fano code added

--- codes/others/shannon_fano.py
+++ codes/others/shannon_fano.py
@@ -1,4 +1,4 @@
-
+import random
 
 def shannon_fano(data):
     alfabet = sorted(data, key = data.get, reverse = True)
@@ -29,5 +29,30 @@ def get_groups(alfabet, data):
     return alfabet[:group_line], alfabet[group_line:]
 
 
+def assert_code(data, ans):
+    test = shannon_fano(data)
+    if test == ans :
+        return True
+    else :
+        return False
+
+
+def generate_to_encode():
+    alfabet = ['a1','a2','a3','a4','a5','a6']
+    data = dict.fromkeys(alfabet, 0)
+    some = 100
+    for i in range(5):
+        if some > 40 : rand = random.randint(1,40)
+        else : rand = random.randint(1,some)
+        data[alfabet[i]] = rand/100
+        some -= rand
+    data[alfabet[5]] = some/100
+    print(sum(list(map(lambda alf : data[alf], alfabet))))
+    return data
+
 # tests
 print(shannon_fano({'a1':0.15, 'a2':0.25, 'a3':0.1, 'a4':0.13, 'a5':0.25, 'a6':0.12}))
+print(assert_code({'a1':0.15, 'a2':0.25, 'a3':0.1, 'a4':0.13, 'a5':0.25, 'a6':0.12}, {'a1':'011','a2':'11','a3':'000','a4':'010','a5':'10','a6':'001'}))
+data = generate_to_encode()
+print(data)
+print(shannon_fano(data))
