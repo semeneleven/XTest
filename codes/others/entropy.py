d66b146e97fd18a12d37c62047f5b89981d0fba4
[semeneleven] <semen.tsinsky@gmail.com>
2018-10-08 15:29:56 +0300

    added entropy algorythms

---
+++ codes/others/entropy.py
@@ -0,0 +1,35 @@
+import math
+
+def inf_quantity(probs, code):
+    inf = 0
+
+    for symbol in code:
+        inf += math.log2(1 / probs[symbol])
+
+    return inf
+
+
+def entropy(probs, max=False):
+    entropy = 0
+
+    for key in probs:
+        entropy -= probs[key]*math.log2(probs[key])
+
+    if max :
+        entropy = math.log2(len(probs))
+
+    return entropy
+
+
+def conditional_entropy(cond_probs, probs, max=False):
+    entropy = 0
+
+    for x in cond_probs:
+        for yx in x:
+            entropy -= yx*math.log2(yx)
+
+    return entropy
+
+    
+# tests
+print(entropy({'a':0.35,'b':0.15,'d':0.3,'c':0.2}, True))
