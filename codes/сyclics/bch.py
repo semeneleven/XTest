1521a4f99e50fca50fd52f40a91f482717e4e639
HalitovskyiSerhii <halit.sergey@gmail.com>
2018-10-08 04:30:42 +0300

    Added BCH and fix Hemming cyclic code.

---
+++ codes/сyclics/bch.py
@@ -0,0 +1,113 @@
+polynomials = {
+    1: [1,0,0,1,1],
+    3: [1,1,1,1,1],
+    5: [1,1,1]
+}
+
+
+def can_correct(R, max_err):
+    start = 0
+    no_more = False
+    while True:
+
+        i = R.find('1', start)
+
+        if i == -1:
+            break
+        elif no_more:
+            return False
+        else:
+            start = i + 1
+
+        count = 0
+        for j in range(i, len(R)):
+            if R[j] == '0':
+                if count >= 1:
+                    no_more = True
+                start = j
+                break
+
+            count += 1
+
+            if count > max_err:
+                return False
+    return True
+
+
+def multiply(a, b):
+    result = (a if int(b[len(b)-1]) else 0)
+    for i in range(len(b)-1):
+        result = (a << (i+1) if int(b[len(b)-i -2]) else 0) ^ (result)
+    return result
+
+
+def bch(msg, d):
+
+    gen_polynomial_num = int("".join([str(x) for x in polynomials[1]]), 2)
+    for i in range(3, d-1, 2):
+            gen_polynomial_num = multiply(gen_polynomial_num, polynomials[i])
+
+    gen_polynomial =bin(gen_polynomial_num)[2:]
+
+    msg += '0'*(15-len(msg))
+    R = msg[msg.find('1'):]
+
+    while len(R) >= len(gen_polynomial):
+        for i in range(len(gen_polynomial)):
+            R = R[:(i)] + str(int(R[i]) ^ int(gen_polynomial[i])) + R[(i + 1):]
+
+        R = R[R.find('1'):]
+
+    R = '0' * (len(gen_polynomial) - len(R)-1) + R
+    return msg[:len(msg) - len(R)] + R
+
+
+def assert_decode(data, answer):
+    msg = data[0]
+    d = data[1]
+    gen_polynomial_num = int("".join([str(x) for x in polynomials[1]]), 2)
+    for i in range(3, d - 1, 2):
+        gen_polynomial_num = multiply(gen_polynomial_num, polynomials[i])
+
+    gen_polynomial = bin(gen_polynomial_num)[2:]
+
+    print(gen_polynomial, d)
+    decoded = msg
+    R = msg
+
+    for i in range(len(decoded) - len(gen_polynomial) + 2):
+
+        while len(R) >= len(gen_polynomial):
+            for j in range(len(gen_polynomial)):
+                R = R[:(j)] + str(int(R[j]) ^ int(gen_polynomial[j])) + R[(j + 1):]
+
+            R = R[R.find('1'):]
+        R = '0' * (len(gen_polynomial) - len(R)-1) + R
+        print(decoded, R)
+        if R.count('1') == 0:
+            break
+        elif can_correct(R,(d-1)//2):
+            for j in range(len(decoded) - len(R), len(decoded) - 1):
+                print(j)
+                decoded = decoded[:j] \
+                          + str(int(R[j % (len(decoded)-len(R))]) ^ int(decoded[j])) \
+                          + decoded[(j + 1):]
+
+            for j in range(len(decoded) - i):
+                decoded = decoded[len(decoded) - 1:] + decoded[:len(decoded) - 1]
+
+            break
+        else:
+            decoded = decoded[len(decoded) - 1:] + decoded[:len(decoded) - 1]
+            R = decoded[decoded.find('1'):]
+
+    decoded = decoded[:len(decoded) - len(gen_polynomial) + 1]
+
+    if not decoded == answer:
+        return False
+
+    return True
+
+
+print(bch('10101', 7))
+print(assert_decode(['111011001000111', 7],'10101'))
--- codes/сyclics/hemming_c.py
+++ codes/сyclics/hemming_c.py
@@ -6,12 +6,44 @@ polynomials = {
 }
 
 
-def hemming(msg, polynomial):
-    polynomial_num = int(''.join([str(x) for x in polynomial]), 2)
+def multiply(a, b):
+    result = (a if int(b[len(b)-1]) else 0)
+    for i in range(len(b)-1):
+        result = (a << (i+1) if int(b[len(b)-i -2]) else 0) ^ (result)
+    return result
 
-    encoded_num = 0
-    for i in polynomial:
-        encoded_num = (encoded_num << 1) ^ (int(msg, 2) if i else 0)
+
+def can_correct(R, max_err):
+    start = 0
+    no_more = False
+    while True:
+
+        i = R.find('1', start)
+
+        if i == -1:
+            break
+        elif no_more:
+            return False
+        else:
+            start = i + 1
+
+        count = 0
+        for j in range(i, len(R)):
+            if R[j] == '0':
+                if count >= 1:
+                    no_more = True
+                start = j
+                break
+
+            count += 1
+
+            if count > max_err:
+                return False
+    return True
+
+
+def hemming(msg, polynomial):
+    encoded_num = multiply(int(msg, 2), polynomial)
 
     encoded = bin(encoded_num)[2:]
     encoded = '0'*(2**(len(polynomial)-1) - len(encoded) -1)+encoded
@@ -42,15 +74,52 @@ def assert_decode(data, answer):
                 R = R[:(j)] + str(int(R[j]) ^ int(polynomial[j])) + R[(j + 1):]
 
             R = R[R.find('1'):]
-        print(decoded, R)
-        decoded = decoded[len(decoded) - 1:] + decoded[:len(decoded) - 1]
-        R = decoded[decoded.find('1'):]
+        R = '0' * (len(polynomial) - len(R) - 1) + R
 
+        if R.count('1') == 0:
+            break
+        elif can_correct(R, 1):
+            for j in range(len(decoded) - len(R), len(decoded) - 1):
+                decoded = decoded[:j] \
+                          + str(int(R[j % (len(decoded) - len(R))]) ^ int(decoded[j])) \
+                          + decoded[(j + 1):]
 
+            for j in range(len(decoded) - i):
+                decoded = decoded[len(decoded) - 1:] + decoded[:len(decoded) - 1]
+
+            break
+        else:
+            decoded = decoded[len(decoded) - 1:] + decoded[:len(decoded) - 1]
+            R = decoded[decoded.find('1'):]
+
+    polynomial_num = int(''.join([str(x) for x in polynomial]), 2)
+
+    result = ''
+
+    while True:
+
+        polynomial_num_tmp =polynomial_num << (len(decoded)-len(polynomial) - decoded.find('1'))
+        result += '0'*decoded.find('1')
+        decoded =decoded[decoded.find('1'):]
+
+        if len(decoded) < len(polynomial):
+            break
+
+        polynomial_tmp = bin(polynomial_num_tmp)[2:]
+
+        for i in range(len(decoded)):
+            decoded = decoded[:i]+str(int(decoded[i])^int(polynomial_tmp[i]))+decoded[i+1:]
+        decoded = decoded[1:]
+        result += '1'
+
+    result += '0'*((len(msg) - len(polynomial)+1)-len(result))
+
+    if not result == answer:
+        return False
 
     return True
 
 
-# print(hemming('01011111010010100010101110', [1, 0, 0, 1, 0, 1]))
-# print(assert_code(['01011111010010100010101110', [1, 0, 0, 1, 0, 1]], '0101011001011001001111111010110'))
-# print(assert_decode(['1001011001011001001111111010110', [1, 0, 0, 1, 0, 1]], '01011111010010100010101110'))
+print(hemming('01011111010', [1, 1, 0, 0, 1]))
+print(assert_code(['01011111010', [1, 1, 0, 0, 1]], '011101010001010'))
+print(assert_decode(['011101010001010', [1, 1, 0, 0, 1]], '01011111010'))
